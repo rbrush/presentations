fe2f306b4effd64b1a46f234edbf1bf2fb250031
Ryan Brush <rbrush@gmail.com>
2014-09-16 09:19:36 -0500

    Update strangeloop demo code.

--- strangeloop-2014/src/strangeloop/demo.clj
+++ strangeloop-2014/src/strangeloop/demo.clj
@@ -1,15 +1,12 @@
 (ns strangeloop.demo
+  "Demo code for Strangeloop 2014."
   (:require [clara.rules :refer :all]
-            [clara.rules.compiler :as com]
-            [clara.rules.accumulators :as acc]
-            [clara.tools.viz :as viz]
             [clara.tools.inspect :as inspect]
             [clara.tools.ui :as ui]
             [clojure.pprint :refer [pprint]]
             [schema.core :as s]
             [clj-time.core :as t])
   (:import [org.joda.time LocalDate]))
-
 
 ;;;;;;;;;;;;;;;;;;
 ;; Supporting functions
@@ -33,32 +30,59 @@
     [name :- s/Str
      birth-date :- LocalDate])
 
-(s/defrecord ChronicKidneyDisease [])
+(s/defrecord ChronicKidneyDisease [severity :- (s/enum :mild :moderate :severe)])
 
-(s/defrecord Referral [type :- (s/enum :nephropathy)])
+(s/defrecord Referral [type :- (s/enum :nephropathy :podiatry :hypertension)
+                       urgency :- (s/enum :low :medium :high)])
+
+(s/defrecord HighRiskPatient [demographics :- Demographics
+                              condition-severity :- (s/enum :mild :moderate :severe)])
 
 ;;;;;;;;;;;;;;;;;;;
 ;; Demo rules.
 
-(defrule mild-hypertensive
-  "Detect mild hypertension."
+(defrule moderate-hypertensive
+  "Detect moderate hypertension."
   [BloodPressure (> 140 systolic 120)]
   =>
   (insert! (->Hypertensive :moderate)))
 
+(defrule severe-hypertensive
+  "Detect severe hypertension."
+  [BloodPressure (>= systolic 140)]
+  =>
+  (insert! (->Hypertensive :severe)))
+
 (defrule senior-bp-meds
   "Recommend appropriate medication for hypertensive patients."
-  [Hypertensive]
+  [Hypertensive (or (= :severe severity)
+                    (= :moderate severity))]
   [Demographics (> (age birth-date) 60)]
   =>
   (insert! (->RecommendedMeds :lisinopril)))
 
 (defrule bp-with-chronic-kidney
-  "Hypertensive with chronic kidney should be actively managed."
-  [Hypertensive]
+  "Hypertensive with chronic kidney should be referred
+   to a nephrologist."
+  [Hypertensive (or (= :severe severity)
+                    (= :moderate severity))]
   [ChronicKidneyDisease]
   =>
-  (insert! (->Referral :nephropathy)))
+  (insert! (->Referral :nephropathy :medium)))
+
+(defrule high-hypertensive-referral
+  "Hypertensive with high severity should be referred to follow up."
+  [Hypertensive (= :severe severity)]
+  =>
+  (insert! (->Referral :hypertension :high)))
+
+(defrule high-risk-patient
+  "Hypertensive with chronic kidney should be actively managed."
+  [ChronicKidneyDisease (= ?severity severity)]
+  [Referral (= :high urgency)]
+  [?demog <- Demographics]
+  =>
+  (insert! (->HighRiskPatient ?demog ?severity)))
 
 (defquery recommended-meds
   "Recommend meds query."
@@ -69,6 +93,11 @@
   "Returns recommended referrals."
   []
   [Referral (= ?type type)])
+
+(defquery risk-by-severity
+  "Query to find high-risk patients with the given condition severity."
+  [:?severity]
+  [?patient <- HighRiskPatient (= ?severity condition-severity)])
 
 ;;;;;;;;;;;;;;;;;;;
 ;; Example usage.
@@ -105,10 +134,13 @@
 ;; But the original is unchanged.
 (pprint (query s1 recommended-meds))
 
-;; Can specify multiple namespaces and create a constant session for speed and simplicity.
-(def empty-session (mk-session 'strangeloop.demo))
+;; And retraction is simple because "equals" actually means something.
+(pprint (query (-> s2
+                   (retract (->BloodPressure 130 100))
+                   (fire-rules))
+               recommended-meds))
 
-;; A macro for convenience:
+;; A macro for easily defining an empty, reusable session.
 (defsession empty-session 'strangeloop.demo)
 
 ;; And use that in a normal function.
@@ -125,7 +157,6 @@
 
       ?type)))
 
-
 (pprint
  (get-medications [(->Demographics "Alice"
                                    (-> 70 t/years t/ago))
@@ -134,31 +165,23 @@
 ;; Rules are data
 (pprint senior-bp-meds)
 
+;; We can generate these from a higher-level DSL.
 (pprint recommended-meds)
 
-;; Rules can be created from a data structure, useful for creating
-;; higher-level declarative DSLs.
-(let [rule-structure {:lhs [{:type 'strangeloop.demo.Demographics
-                             :constraints '[(= ?age (age birth-date))
-                                            (> (age birth-date) 60)]}]}
-
-      session (mk-session [rule-structure])]
-
-  (pprint (-> session
-              (insert (->Demographics "Alice"
-                                      (-> 70 t/years t/ago)))
-              (fire-rules)
-              (query rule-structure))))
-
 ;; And we can inspect session state as well.
-(let [session (-> (mk-session)
-                  (insert (->Demographics "Alice"
-                                          (-> 70 t/years t/ago))
-                          (->BloodPressure 130 100)
-                          (->ChronicKidneyDisease))
-                  (fire-rules))]
+(def inspected-session
+  (-> (mk-session :cache false)
+      (insert (->Demographics "Alice"
+                              (-> 70 t/years t/ago))
+              (->BloodPressure 150 120)
+              (->ChronicKidneyDisease :severe))
+      (fire-rules)))
 
-  (ui/show-session session))
+;; From the console...
+(inspect/explain-activations inspected-session)
+
+;; ...and visually
+(ui/show-session inspected-session)
 
 ;;;;;;;;;;;;;;;;;;;
 ;; Vizualization demo
